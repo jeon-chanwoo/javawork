98ecda3c2c3138a7159ab6080a86f63b7acf688a
jeon-chanwoo <63829000+jeon-chanwoo@users.noreply.github.com>
2024-05-07 13:17:19 +0900

    day1 commit

---
+++ day01/src/day01/T01_print.java
@@ -0,0 +1,21 @@
+package day01;
+
+public class T01_print {
+	public static void main(String[] args) {
+		// 이것은 주석이라우
+		/*
+		  이것도 주석이라우
+		*/
+		//기본 출력문
+		//문자는 ""안에 써야한다.
+		System.out.println("정보처리기사 실기 공부 열심히 하자");
+		System.out.println("전찬우입니다");
+		System.out.println("전찬우입니다"+5*3);
+		System.out.println("전찬우입니다"+5*3+"강의실 801호");
+		System.out.println("전찬우입니다"+5*3+"강의실 801호"+(5+8));
+		System.out.println("전찬우입니다"+5*3+"강의실 801호"+5*8);
+
+	}
+	
+
+}
