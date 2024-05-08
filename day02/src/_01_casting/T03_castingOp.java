66bcb92077d0fec342fdccc780e720ff3ac1e423
jeon-chanwoo <63829000+jeon-chanwoo@users.noreply.github.com>
2024-05-08 14:14:23 +0900

    Update T03_castingOp.java

--- day02/src/_01_casting/T03_castingOp.java
+++ day02/src/_01_casting/T03_castingOp.java
@@ -11,7 +11,21 @@ public class T03_castingOp {
 		int result = b1+ b2;
 		
 		byte result2 = (byte)(b1+b2);
-dfasdfasdfdsaf
+		
+		char c1 = 'A';
+		int c3 = c1 +1;
+		char c2 = (char)(c1+1);
+		System.out.println(c2);
+		System.out.println(c3);
+	
+		int num1 = 100;
+		int num2 = 3;
+		int result3 = num1/num2;
+		System.out.println(result3);
+		
+		double dou2 = 3;
+		double result4 = num1/ dou2;
+		System.out.println(result4);
 	}
 
 }
