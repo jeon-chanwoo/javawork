cffea0b816496d0f9d7dc6451e7c03464b06479e
jeon-chanwoo <63829000+jeon-chanwoo@users.noreply.github.com>
2024-05-10 10:06:46 +0900

    Update T04_stack_if.java

--- day03/src/_02_conditional/T04_stack_if.java
+++ day03/src/_02_conditional/T04_stack_if.java
@@ -11,14 +11,9 @@ public class T04_stack_if {
 		Scanner sc1 = new Scanner(System.in);
 		System.out.println("점수를 입력해 주세요");
 		int num1 = sc1.nextInt();
-		if()
-			if()
-				System.out.println("");
-			else	
-		else 
-			if()
-				System.out.println("");
-			else 
+		/*
+		 * if() if() System.out.println(""); else else if() System.out.println(""); else
+		 */
 		
 	}
 }
