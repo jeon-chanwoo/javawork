2703120af88181039a9d2ffed23e9fb226ecfb38
jeon-chanwoo <63829000+jeon-chanwoo@users.noreply.github.com>
2024-05-13 13:38:45 +0900

    Update T01_1array.java

--- day05/src/_01_array/T01_1array.java
+++ day05/src/_01_array/T01_1array.java
@@ -4,7 +4,18 @@ public class T01_1array {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] arrayInt = new int[10];
+		arrayInt[0] = 10;
+		arrayInt[2] = 20;
+		
+		for(int i = 0;i<5;i++)
+			System.out.println(arrayInt[i]);
+		
+		int[] arrayInt2 = {1,2,3,4,5};
+		
+		for(int i = 0;i<5;i++)
+			System.out.println(arrayInt2[i]);
+		
 	}
 
 }
