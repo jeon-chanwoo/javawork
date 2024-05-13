e9cc22d49372afbe74f5a1fcc7a8a9615ed4222f
jeon-chanwoo <63829000+jeon-chanwoo@users.noreply.github.com>
2024-05-13 15:03:33 +0900

    Update T02_2array.java

--- day05/src/_01_array/T02_2array.java
+++ day05/src/_01_array/T02_2array.java
@@ -7,24 +7,16 @@ public class T02_2array {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc1 = new Scanner(System.in);
-		int[] arrayInt = new int[10];
-		arrayInt[0] = 10;
-		arrayInt[2] = 20;
-		
-		for(int i = 0;i<5;i++)
-			System.out.println(arrayInt[i]);
-		
-		int[] arrayInt2 = {1,2,3,4,5};
-		
-		for(int i = 0;i<5;i++)
-			System.out.println(arrayInt2[i]);
-		
-		char[] arrayChar = new char[10];
-		for(int i=0; i<10;i++) {
-		arrayChar[i] = sc1.next().charAt(0);
-		}
-		
-		
-	}
+		int[][] score = new int[3][5];
 
+		int num = 1;
+		for (int i = 0; i < score.length; i++) {
+			for (int j = 0; j < score[i].length; j++) {
+				score[i][j] = num;
+				System.out.print(score[i][j]+"\t");
+				num += 2;
+			}
+			System.out.println();
+		}
+	}
 }
