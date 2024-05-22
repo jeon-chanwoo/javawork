94b409364ed6857555369e2b2249df72ee6a0362
jeon-chanwoo <63829000+jeon-chanwoo@users.noreply.github.com>
2024-05-22 12:17:13 +0900

    day10

---
+++ day11/src/_01_anonymous/TestMain.java
@@ -0,0 +1,35 @@
+package _01_anonymous;
+
+class Parent {
+	String method() {
+		return "부모 클래스";
+	}
+}
+
+class Child extends Parent {
+	@Override
+	String method() {
+		return "자식 클래스";
+	}
+}
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Parent p = new Child();
+		System.out.println(p.method());
+
+		// 클래스에 들어가는 것을 재정의 한다.
+		Parent p2 = new Child() {
+			int age = 25;
+			@Override
+			String method() {
+				System.out.println(age);
+				return "자식 클래스2";
+			}
+		};
+		System.out.println(p2.method());
+	}
+
+}
