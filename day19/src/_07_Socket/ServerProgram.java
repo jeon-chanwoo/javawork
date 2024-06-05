25b597070284be6fc789e8415e377c81d23bc459
jeon-chanwoo <63829000+jeon-chanwoo@users.noreply.github.com>
2024-06-05 13:53:30 +0900

    day19

--- day19/src/_07_Socket/ServerProgram.java
+++ day19/src/_07_Socket/ServerProgram.java
@@ -13,43 +13,27 @@ import java.util.Scanner;
 public class ServerProgram {
 	public static void main(String[] args) {
 		int port = 9001;
-		Scanner sc = new Scanner(System.in);
-		BufferedReader br = null;	
-		PrintWriter pw = null;		
-		ServerSocket server = null;
+		Scanner sc = new Scanner(System.in);	
 		
 		try {
-			try() {
-				server = new ServerSocket(port);
+			try(ServerSocket server = new ServerSocket(port)) {
 				System.out.println("클라이언트의 요청을 기다리고 있음...");
 				
-				// 4) 연결 요청이 들어오면 수락 후 해당 클라이언트와 통신할 수 있는 서버측 소켓 객체 생성
 				Socket socket = server.accept(); 
 				System.out.println(socket.getInetAddress().getHostAddress() + "가 연결요청함!");
-				/*
-				// 5) 클라이언트와 입출력 기반스트림 생성(바이트 스트림밖에 안됨)
-				InputStream is = socket.getInputStream();
-				OutputStream os = socket.getOutputStream();
+				try(BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+						PrintWriter pw = new PrintWriter(socket.getOutputStream())){
 				
-				// 6) 성능향상을 위해 보조스트림 사용
-				br = new BufferedReader(new InputStreamReader(is));
-				*/
-				br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				pw = new PrintWriter(socket.getOutputStream());
-				// 데이터 출력시 print(), println() 메소드를 가지고 있는 보조스트림
-				
-				pw.println("환영합니다!!! 🐱‍💻");
-				pw.flush();
-				
-				while(true) {
-					// 7) 스트림을 통해 읽고 쓰기
-					// 클라이언트측의 메시지 읽기
-					System.out.println("클라이언트 : " + br.readLine());
-					
-					// 클라이언트에게 메시지 보내기
-					System.out.print("서버 : " );
-					pw.println(sc.nextLine());
+					pw.println("환영합니다!!! 🐱‍💻");
 					pw.flush();
+					
+					while(true) {
+						System.out.println("클라이언트 : " + br.readLine());
+						
+						System.out.print("서버 : " );
+						pw.println(sc.nextLine());
+						pw.flush();
+					}
 				}
 			}
 		} catch (IOException e) {
